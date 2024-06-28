d4ea9304f5810a10a7daf52d616369d8efae75e9
jmw <walkerjulian888@gmail.com>
2024-06-28 16:23:30 +0100

    Updated HTML CSS JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -91,3 +91,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   toggleSwitch.addEventListener('change', switchTheme, false);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('.section');
+  const navLinks = document.querySelectorAll('.nav-links a');
+
+  function onScroll() {
+    let current = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
+      if (link.getAttribute('href').slice(1) === current) {
+        link.classList.add('active');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', onScroll);
+});
